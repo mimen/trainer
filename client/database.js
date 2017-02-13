48e02d50c5e352fd9b5a2637a86e4dd5039f52a5
susansshen <ucsd2016grad@gmail.com>
2017-02-12 18:48:50 -0800

    signup user changes

--- client/database.js
+++ client/database.js
@@ -3,3 +3,20 @@ var config = require('../config.js')
 var options = {};
 var pgp = require('pg-promise')(options);
 var db = pgp(config.postgres_url);
+
+function addUser(username, password) {
+
+  var query = `INSERT INTO USERS(username, password)
+               VALUES (${username}, ${password})`;
+
+  console.log(query);
+  // db.query(query);
+}
+
+// -- INSERT INTO users(username, password)
+// -- VALUES ("milad", "poop")
+// -- RETURNING id;
+
+module.exports = {
+  addUser: addUser
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../config.js');
+const config = require('../config');
+
+const database = require('../client/database');
 
 router.get('/', function(req, res, next) {
   res.render('home', {});
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {});
+});
+
+router.get('/signup/submit', function(req, res, next) {
+  const username = req.query.username;
+  const password = req.query.password;
+  database.addUser(username, password);
+
+  res.render('submitted', {});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
 });
 
 module.exports = router;
